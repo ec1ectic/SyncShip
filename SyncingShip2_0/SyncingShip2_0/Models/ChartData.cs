787943493976b2c0e953776c2e1d548856800e53
Adam <adam@trimeloni.com>
2013-10-17 15:00:34 -0500

    Added C Sharp REST API

---
+++ SyncingShip2_0/SyncingShip2_0/Models/ChartData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SyncingShip2_0.Models
+{
+    public class ChartData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string TemplateName { get; set; }
+        public string JSonPayload { get; set; }
+    }
+}
---
+++ SyncingShip2_0/SyncingShip2_0/Models/ChartDataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SyncingShip2_0.Models
+{
+    public class ChartDataRepository : IChartData
+    {
+        public ChartData Add(ChartData item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            // TODO: Integrate this with Jordan
+
+
+            return item;
+        }
+
+    }
+}
